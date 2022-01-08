f814317303c0e2bade81df97936c5d331096b20a
Gdurro <grzegorzchra@gmail.com>
2022-01-09 00:02:44 +0100

    main code of perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,1 +1,20 @@
-print("test")
+x_input = [0.1, 0.5, 0.2]
+w_weights = [0.4, 0.3, 0.6]
+treshold = 0.5
+
+def step(weighted_sum):
+    if weighted_sum > treshold:
+        return 1
+    else:
+        return 0
+
+def perceptron():
+    weighted_sum = 0
+    for x,w in zip(x_input, w_weights):
+        weighted_sum += x*w
+        print(weighted_sum)
+    return step(weighted_sum)
+
+
+output = perceptron()
+print("output: " + str(output))
